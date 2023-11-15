037dd060b64316f3dcd5349017bb51e0a98af6ae
Rodrigo Lessyn de Paula <rodrigolessyn@gmail.com>
2023-11-15 13:51:56 -0300

    Componentes e props

---
+++ react/projeto-react/src/App.jsx
@@ -0,0 +1,26 @@
+import MeuComponente from "./components/MeuComponente"
+
+function App() {
+  
+  return (
+    <div>
+
+      <h1>Olá Mundo, React!</h1>
+      <MeuComponente/> 
+
+      <MeuBotao conteudo = 'Me clique'/> 
+
+      <MeuBotao conteudo = 'Depois aqui'/> 
+
+      <MeuBotao conteudo = 'e por fim aqui'/>   
+    </div>
+  )
+}
+
+function MeuBotao(props){
+  return(
+    
+    <button>{props.conteudo}</button>
+  )
+}
+export default App
---
+++ react/projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,5 @@
+export default function MeuComponente(){
+    return (
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptatem nobis officiis quibusdam culpa exercitationem magnam, saepe sequi id recusandae, fugit omnis sed labore, facilis molestiae! Ipsam soluta dolore aliquid.</p>
+    )
+  }
